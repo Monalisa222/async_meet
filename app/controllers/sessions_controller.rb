876621c0fc0809bb296fe7c0f2e4c6b057e3e837
Monalisa <monalisadengre222@gmail.com>
2026-02-23 12:17:14 +0530

    Add SessionsController with login and logout login

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,20 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:email])
+    if user&.authenticate(params[:password]) && user.active
+      session[:user_id] = user.id
+      redirect_to organizations_path, notice: "Logged in successfully."
+    else
+      flash.now[:alert] = "Invalid email or password."
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    reset_session
+    redirect_to login_path, notice: "Logged out successfully."
+  end
+end
